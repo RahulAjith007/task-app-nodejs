07118b703b8dc03e9cf1cedfb3b0a45404eeddd0
Rahul Ajith <RahulAjith007>
2020-11-22 18:24:10 +0530

    added jwt

--- src/routers/task.js
+++ src/routers/task.js
@@ -86,10 +86,11 @@ router.patch('/tasks/:id', async(req, res) => {
         return res.status(400).send({error: "Invalid Updates!"})
     }
 
-
-
     try{
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators:true})
+        const id = req.params.id;
+        const task = await Task.findById(id);
+        updates.forEach((update)=> task[update] = req.body[update]);
+        await task.save();
         if(!task){
             return res.status(404).send()
         }
